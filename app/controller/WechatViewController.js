cc72db329a73e38033a9c65c4d1b0512d37d1082
HarryFan <harrisfan@eskygo.com>
2015-09-09 17:55:39 +0800

    update harry views

--- app/controller/WechatViewController.js
+++ app/controller/WechatViewController.js
@@ -6,9 +6,44 @@ var WechatAPI = require('co-wechat-api'),
 exports = module.exports = function(app) {
     var m = app.models;
     app.group('/wechat-view', function(router) {
-        router.get('/car', function*(next){
-            yield this.render('wechat-view/car.html', {token:this.query['t']});
+        router.get('/car', function*(next) {
+            yield this.render('wechat-view/car.html', {
+                token: this.query['t']
+            });
         });
+
+        router.get('/ParkingPayment', function*(next) {
+            yield this.render('wechat-view/ParkingPayment.html', {
+                token: this.query['t']
+            });
+        });
+
+        router.get('/ParkingPaymentOverTime', function*(next) {
+            yield this.render('wechat-view/ParkingPaymentOverTime.html', {
+                token: this.query['t']
+            });
+        });
+
+        router.get('/parkingRecord', function*(next) {
+            yield this.render('wechat-view/parkingRecord.html', {
+                token: this.query['t']
+            });
+        });
+
+        router.get('/paychoice', function*(next) {
+            yield this.render('wechat-view/paychoice.html', {
+                token: this.query['t']
+            });
+        });
+        router.get('/UseCouponForParkingPayment', function*(next) {
+            yield this.render('wechat-view/UseCouponForParkingPayment.html', {
+                token: this.query['t']
+            });
+        });
+
+
+
+
     });
 
 }
