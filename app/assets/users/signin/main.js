0cc4f3ced460c237822a5434543bb31e8b0b2799
Ting-An Chou <joanne3634@gmail.com>
2015-09-18 02:26:04 +0800

    不小心自己rebase掉一些code 修復

--- app/assets/users/signin/main.js
+++ app/assets/users/signin/main.js
@@ -1,21 +1,29 @@
-define(['jquery'], function($){
-    // submit
-    $('#RegisterForm').on('submit',function(e){
-        e.preventDefault();
+define(['jquery'], function($) {
+    $('#RegisterForm').on('submit', function(e) {
 
         /* 手机号码錯誤 */
-        var phoneStatus = true;
-        if( !phoneStatus ){
-            console.log()
-            Alert('请输入正确的手机号码','好的');
+        var Phone = document.getElementById('Phone').value;
+
+        if ($.trim(Phone) === "") {
+            Alert('手机不能为空', '好的');
+            e.preventDefault();
+            return false;
+        } else if (Phone.length !== 11) {
+            Alert('手机号码不能小于11位', '好的');
+            e.preventDefault();
+            return false;
+        } else if (!/^(1[^012][0-9]{9})$/i.test(Phone)) {
+            Alert('手机号码格式不正确', '好的');
+            e.preventDefault();
             return false;
         }
 
         var validateCode = document.getElementById('ValidateCode');
-        var validateCodeStatus = true;
+        var validateCodeStatus = true; // TODO: connect API 
+
         /* 驗證碼錯誤 */
-        if( !validateCodeStatus ){
-            Alert('请输入正确的驗證码','好的');
+        if (!validateCodeStatus) {
+            Alert('请输入正确的驗證码', '好的');
             return false;
         }
 
@@ -28,56 +36,83 @@ define(['jquery'], function($){
         // }
 
         var registerStatus = true;
-        if( registerStatus ){
-            //TODO: redirect to wechat-login page
+        if (registerStatus) {
+            //TODO: redirect to wechat-login page 
         }
 
     });
 
-    function Alert(dialog,check){
+    var c = 60;
+    var t;
+
+    $(document).on('click', "#GetCode", function(e) {
+        // TODO : connect API for get validation code 
+        // code validation time : 10 min
+        timedCount();
+    })
+
+    function timedCount() {
+        document.getElementById('GetCode').innerHTML = c + '秒';
+        document.getElementById('GetCode').disabled = true;
+        c--;
+        t = setTimeout(function() {
+            timedCount();
+        }, 1000);
+        if (c < 0) {
+            stopCount();
+        }
+    }
+
+    function stopCount() {
+        c = 60;
+        document.getElementById('GetCode').disabled = false;
+        setTimeout("document.getElementById('GetCode').innerHTML='获取验证码'", 0);
+        clearTimeout(t);
+    }
+
+    function Alert(dialog, check) {
         var winW = window.innerWidth;
         var winH = window.innerHeight;
         var dialogoverlay = document.getElementById('dialogoverlay');
         var dialogbox = document.getElementById('dialogbox');
         dialogoverlay.style.display = "block";
-        dialogoverlay.style.height = winH+"px";
-        dialogbox.style.left = (winW/2) - (280 * .5)+"px";
+        dialogoverlay.style.height = winH + "px";
+        dialogbox.style.left = (winW / 2) - (280 * .5) + "px";
         dialogbox.style.top = "100px";
         dialogbox.style.display = "block";
         $('#dialogboxhead').html("eGo提示");
         $('#dialogboxbody').html(dialog);
-        $('#dialogboxfoot').html('<button id="AlertButton" class="ui button alert">'+check+'</button>');
+        $('#dialogboxfoot').html('<button id="AlertButton" class="ui button alert">' + check + '</button>');
     }
-    $(document).on('click', "#AlertButton", function(e){
+    $(document).on('click', "#AlertButton", function(e) {
         e.preventDefault();
         document.getElementById('dialogbox').style.display = "none";
         document.getElementById('dialogoverlay').style.display = "none";
-    });
+    })
 
-    function ConfirmCarBindingStatus (dialog,left,right){
+    function ConfirmCarBindingStatus(dialog, left, right) {
         var winW = window.innerWidth;
         var winH = window.innerHeight;
         var dialogoverlay = document.getElementById('dialogoverlay');
         var dialogbox = document.getElementById('dialogbox');
         dialogoverlay.style.display = "block";
-        dialogoverlay.style.height = winH+"px";
-        dialogbox.style.left = (winW/2) - (280 * .5)+"px";
+        dialogoverlay.style.height = winH + "px";
+        dialogbox.style.left = (winW / 2) - (280 * .5) + "px";
         dialogbox.style.top = "100px";
         dialogbox.style.display = "block";
 
         $('#dialogboxhead').html("eGo提示");
         $('#dialogboxbody').html(dialog);
-        $('#dialogboxfoot').html('<button id="ConfirmLeftButton" class="ui button confirm" >'+left+'</button> <button id="ConfirmRightButton" class="ui button confirm">'+right+'</button>');
+        $('#dialogboxfoot').html('<button id="ConfirmLeftButton" class="ui button confirm" >' + left + '</button> <button id="ConfirmRightButton" class="ui button confirm">' + right + '</button>');
     }
 
-    $(document).on('click', "#ConfirmLeftButton", function(e){
+    $(document).on('click', "#ConfirmLeftButton", function(e) {
         document.getElementById('dialogbox').style.display = "none";
         document.getElementById('dialogoverlay').style.display = "none";
-    });
-    $(document).on('click', "#ConfirmRightButton", function(e){
-        document.location = document.location.origin+'/wechat-view/carAuth';
+    })
+    $(document).on('click', "#ConfirmRightButton", function(e) {
+        document.location = document.location.origin + '/wechat-view/carAuth';
         document.getElementById('dialogbox').style.display = "none";
         document.getElementById('dialogoverlay').style.display = "none";
-    });
-
+    })
 });
--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -24,43 +24,87 @@ exports = module.exports = function(app) {
     var m = app.models;
 
 
-    app.group('/users', function(router){
-        router.get('/signin', function*(next){
+    app.group('/users', function(router) {
+        router.get('/index', function*(next) {
+            yield this.render('users/index.html')
+        });
+        router.get('/signin', function*(next) {
             yield this.render('users/signin.dust')
         });
-        router.post('/signup', function*(next){
-            var self = this, dustFile = 'users/v2/signup.dust', dian13User , eSkyGoUser;
+        router.get('/car', function*(next) {
+            yield this.render('users/car.dust')
+        });
+        router.get('/carAuth', function*(next) {
+            yield this.render('users/carAuth.dust')
+        });
+        router.get('/afterCarAuth', function*(next) {
+            yield this.render('users/afterCarAuth.dust')
+        });
+        router.get('/coupon', function*(next) {
+            yield this.render('users/coupon.dust')
+        });
+        router.get('/couponForPay', function*(next) {
+            yield this.render('users/couponForPay.dust')
+        });
+        router.get('/help', function*(next) {
+            yield this.render('users/help.dust')
+        });
+        router.get('/pay', function*(next) {
+            yield this.render('users/pay.dust')
+        });
+        router.get('/paychoice', function*(next) {
+            yield this.render('users/paychoice.dust')
+        });
+        router.get('/record', function*(next) {
+            yield this.render('users/record.dust')
+        });
+        router.get('/recordDetail', function*(next) {
+            yield this.render('users/recordDetail.dust')
+        });
+        router.post('/signup', function*(next) {
+            var self = this,
+                dustFile = 'users/v2/signup.dust',
+                dian13User, eSkyGoUser;
             try {
                 var params = yield this.req.body();
-                if(params.channel && params.channel === "welcome") {
+                if (params.channel && params.channel === "welcome") {
                     dustFile = 'home/welcome.dust';
                 }
                 delete params.channel;
-                if ( !params.username || !params.password) {
+                if (!params.username || !params.password) {
                     return yield this.render(dustFile, {
                         error: "请填写信息"
                     });
                 }
-                if ( params.username === "" || params.password === "" ) {
+                if (params.username === "" || params.password === "") {
                     return yield this.render(dustFile, {
                         error: "请填写信息"
                     });
                 }
 
-                var alias = {type: "username", alias: params.username, provider: "eskygo", password: app._crypter.hash(params.password) };
+                var alias = {
+                    type: "username",
+                    alias: params.username,
+                    provider: "eskygo",
+                    password: app._crypter.hash(params.password)
+                };
 
                 var checkRes = yield userServ.post('/users/check', alias, {});
                 checkRes = JSON.parse(checkRes.body);
 
-                if(checkRes.res && checkRes.userId) {
-                    dian13User = yield m.Dian13User.findOne({"eskygo_user":"#"+checkRes.userId}).exec();
-                    if(dian13User) {
+                if (checkRes.res && checkRes.userId) {
+                    dian13User = yield m.Dian13User.findOne({
+                        "eskygo_user": "#" + checkRes.userId
+                    }).exec();
+                    if (dian13User) {
                         return yield this.render(dustFile, {
                             error: "用户已存在"
                         });
                     }
-                    eSkyGoUser = {id: checkRes.userId};
-                } else if(!checkRes.res && checkRes.userId) {
+                    eSkyGoUser = {
+                        id: checkRes.userId
+                    };
+                } else if (!checkRes.res && checkRes.userId) {
                     // user exist, but it is wrong password
                     return this.status = 409;
                 } else {
@@ -69,25 +113,34 @@ exports = module.exports = function(app) {
                         mobile: params.username,
                         password: params.password,
                         area: params.area,
-                        alias: {type: "username", alias: params.username, provider: "eskygo"},
-                        provider: {type: "eskygo", data:''}
+                        alias: {
+                            type: "username",
+                            alias: params.username,
+                            provider: "eskygo"
+                        },
+                        provider: {
+                            type: "eskygo",
+                            data: ''
+                        }
                     }, {});
                     eSkyGoUser = JSON.parse(eSkyGoUser.body);
                 }
 
                 try {
                     dian13User = new m.Dian13User({
-                        eskygo_user: '#'+eSkyGoUser.id,
+                        eskygo_user: '#' + eSkyGoUser.id,
                         last4_number: params.last4_number
                     });
                     dian13User = yield dian13User.save();
-                    yield userServ.post(`/users/`+eSkyGoUser.id+`/roles`, {roles: ["13dian"]});
-                } catch(e) {
-                    if ( 'com.orientechnologies.orient.core.storage.ORecordDuplicatedException' !== e.type ) {
+                    yield userServ.post(`/users/` + eSkyGoUser.id + `/roles`, {
+                        roles: ["13dian"]
+                    });
+                } catch (e) {
+                    if ('com.orientechnologies.orient.core.storage.ORecordDuplicatedException' !== e.type) {
                         throw e;
                     } else {
                         dian13User = m.Dian13User.findOne({
-                            eskygo_user: '#'+eSkyGoUser.id
+                            eskygo_user: '#' + eSkyGoUser.id
                         }).exec();
                     }
                 }
@@ -99,14 +152,14 @@ exports = module.exports = function(app) {
                 eSkyGoUser.provider = "eskygo";
                 eSkyGoUser.alias = params.username;
 
-                yield (new Promise(function(resolve, reject){
+                yield(new Promise(function(resolve, reject) {
                     debug('signup success logging in user now')
-                    self.req.login(eSkyGoUser, function(err){
-                        if ( token ) {
+                    self.req.login(eSkyGoUser, function(err) {
+                        if (token) {
                             debug('token exists');
-                            JWT.verify(token).then(function(obj){
-                                if ( obj.origUrl) {
-                                    debug('redirecting to '+ obj.origUrl);
+                            JWT.verify(token).then(function(obj) {
+                                if (obj.origUrl) {
+                                    debug('redirecting to ' + obj.origUrl);
                                     self.redirect(obj.origUrl);
                                 }
                                 resolve();
@@ -119,9 +172,9 @@ exports = module.exports = function(app) {
                         debug('no token');
                     });
                 }));
-            } catch(e) {
+            } catch (e) {
                 debug("Failed to signup user", e, e.stack);
-                return this.status=500;
+                return this.status = 500;
             }
         });
     })
